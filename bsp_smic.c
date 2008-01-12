ebfc5ab82ed6749cff668c37de4efc139f3912e4
till <>
2008-01-12 00:53:17 +0000

    - added support for IPMI watchdog on PCs (requires separate 'smic' driver!)

---
+++ bsp_smic.c
@@ -0,0 +1,53 @@
+/* $Id$ */
+
+/* Watchdog hardware support for PCs with IPMI/SMIC watchdog.
+ * by Till Straumann <strauman@slac.stanford.edu>, Jan. 2007
+ */
+
+#ifdef __rtems__
+#include <rtems.h>
+#endif
+
+
+#include <stdio.h>
+
+#include "wd.h"
+
+#define DEBUG	0
+
+#if	DEBUG > 0
+#define STATIC
+#else
+#define STATIC static
+#endif
+
+extern unsigned short wArmWatchdog(unsigned);
+extern unsigned short wResetWatchdog();
+
+void
+wdInit(unsigned long us)
+{
+	/* wArmWatchdog interval is in 100ms units */
+	us = us/100000 + 1;
+	wArmWatchdog(us);
+	/* must pet once to start */
+	wResetWatchdog();
+}
+
+void
+wdHalt(void)
+{
+	wArmWatchdog(0);
+}
+
+void
+wdPet(void)
+{
+	wResetWatchdog();
+}
+
+void
+wdSysReset(void)
+{
+	rtemsReboot();
+}
