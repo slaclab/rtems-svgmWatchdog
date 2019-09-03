8f70be7616ecb9ddac15fe5bdb5ae34fabab8cb3
till straumann <till.straumann@alumni.tu-berlin.de>
2019-09-03 12:54:24 -0700

    Resolved previous conflicts from merging cvs_2_git branch

--- wdclnt.c
+++ wdclnt.c
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
-=======
->>>>>>> cvs_name_2_gitrev
 #include <rpc/rpc.h>
 #include <stdio.h>
 #include <netdb.h>
@@ -18,11 +15,7 @@
 static void
 usage(char *n)
 {
-<<<<<<< HEAD
-	fprintf(stderr,"%s version %s\n", n, PACKAGE_VERSION);
-=======
 	fprintf(stderr,"Git revision %s\n", PACKAGE_VERSION);
->>>>>>> cvs_name_2_gitrev
 	fprintf(stderr,"usage: %s [-rvh] target\n",n);
 	fprintf(stderr,"          -r: reset now and quit\n");
 	fprintf(stderr,"          -v: print version number and quit\n");
